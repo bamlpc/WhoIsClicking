8b33885b40778957f45f8d5757fe83774ac5ed36
bmogetta <brunomogetta@gmail.com>
2022-05-06 19:08:53 -0300

    preparando denon

--- mod.ts
+++ mod.ts
@@ -1,1 +1,17 @@
 // entry point for the proyect
+import {Application, Router} from "./deps.ts";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (ctx) => {
+    ctx.response.body = ctx.request.ip + " 12" + ctx.request.method;
+} )
+
+console.log("Running....");
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+await app.listen({ port: 8000});
+
+
