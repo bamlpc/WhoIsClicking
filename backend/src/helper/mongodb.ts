fab007c6ae8cdf29745fe4a253a7fa7c8c64f6a4
Bruno Mogetta <brunomogetta@gmail.com>
2022-06-03 18:03:45 -0300

    change mongo console to mongosh

--- backend/src/helper/mongodb.ts
+++ backend/src/helper/mongodb.ts
@@ -6,6 +6,7 @@ const DB_NAME = Deno.env.get('DB_NAME');
 const DB_USER = Deno.env.get('MONGODB_USER');
 const DB_PASS = Deno.env.get('MONGODB_PASS');
 //const MONGO_URL = Deno.env.get('MONGODB_URL');
+// this is what we should use "mongodb://<username>:<password>@localhost:27017/<authDB>");
 const MONGO_CONNECTION =  `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.adchc.mongodb.net/?authMechanism=SCRAM-SHA-1`;
 
 await client.connect(MONGO_CONNECTION);
