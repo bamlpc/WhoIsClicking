1aae5e2ba37ff0807c28e25f6e968c7b259cb390
Bruno Mogetta <brunomogetta@gmail.com>
2022-06-03 22:38:06 -0300

    finished mongo-init.sh script, db working

--- backend/src/helper/mongodb.ts
+++ backend/src/helper/mongodb.ts
@@ -5,8 +5,9 @@ const client = new MongoClient( );
 const DB_NAME = Deno.env.get('DB_NAME');
 const DB_USER = Deno.env.get('MONGODB_USER');
 const DB_PASS = Deno.env.get('MONGODB_PASS');
-//const MONGO_URL = Deno.env.get('MONGODB_URL');
-// this is what we should use "mongodb://<username>:<password>@localhost:27017/<authDB>");
+const MONGO_URL = Deno.env.get('MONGODB_URL');
+// dockerDB connection should look like this:
+//const MONGO_CONNECTION = `mongodb+srv://${DB_USER}:${DB_PASS}@${MONGO_URL}/${DB_NAME}?authMechanism=SCRAM-SHA-1`;
 const MONGO_CONNECTION =  `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.adchc.mongodb.net/?authMechanism=SCRAM-SHA-1`;
 
 await client.connect(MONGO_CONNECTION);
--- mongodb/mongo-init.sh
+++ mongodb/mongo-init.sh
@@ -1,10 +1,20 @@
+echo '                                                        '
+echo '########################################################'
+echo '################ MONGO ENTRYPOINT START ################'
+echo '########################################################'
+echo '                                                        '
 mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
-    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
-    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
-    var admin = db.getSiblingDB('admin');
-    admin.auth(rootUser, rootPassword);
-
-    var user = '$MONGO_INITDB_USERNAME';
-    var passwd = '$MONGO_INITDB_ROOT_PASSWORD';
-    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+    var admin = db.getSiblingDB("admin");
+    admin.auth("$MONGO_INITDB_ROOT_USERNAME", "$MONGO_INITDB_ROOT_PASSWORD");
+    db.createUser({
+        user: "$MONGO_INITDB_USERNAME", 
+        pwd: "$MONGO_INITDB_PASSWORD", 
+        roles: [{role: "dbAdmin", db: "$MONGO_INITDB_DATABASE"}]});
+    db.createCollection('users');
 EOF
+echo '                                                        '
+echo '                                                        '
+echo '########################################################'
+echo '################ MONGO ENTRYPOINT END ##################'
+echo '########################################################'
+echo '                                                        '
