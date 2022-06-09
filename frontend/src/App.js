420215330a7ff64cf8840cb68866fa7a8d0ff637
Bruno Mogetta <brunomogetta@gmail.com>
2022-06-09 14:21:02 -0300

    frontend router working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
+import {Routes, Route } from 'react-router-dom';
+import {Home} from './routes/Home.js';
+import {Generated} from './routes/Generated.js';
 
-const Button = styled.button `
-  background-color: #3d5afe;
-  font-size: 1.5em;
-  color: white;
-  padding: 20px 30px;
-  border-radius: 8px;
-  outline: 1;
-  box-shadow: 0 2px 2px 0 #263238;
-  cursor: pointer;
-  transition: ease background-color 0.25s;
-  &:hover {
-    background-color: #304ffe;`
-
-let links;
-function Create_Link() {
-  API_call();
-  alert('Educational propose only');
-  }
-function API_call() {
-  links = fetch('http://localhost:81/') || "Not working";
-  console.log(links)
-};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         <Button onClick={Create_Link}> Create Link </Button>
-        </p>
-        <p>{links}</p>
-      </header>
-    </div>
+    <>
+        <Routes>
+          <Route path='/' element={<Home />}                /> 
+          <Route path='/generated' element={<Generated />}  /> 
+        </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/routes/Generated.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+export const Generated = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+           "link generated"
+          </p>
+        </header>
+      </div>
+    );
+  }
