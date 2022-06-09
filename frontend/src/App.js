46ec4f3425f1d0604e5bf583444bc9b3015b87f8
Bruno Mogetta <brunomogetta@gmail.com>
2022-06-08 21:02:18 -0300

    linked frontend-nginx-backend-database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Button = styled.button `
+  background-color: #3d5afe;
+  font-size: 1.5em;
+  color: white;
+  padding: 20px 30px;
+  border-radius: 8px;
+  outline: 1;
+  box-shadow: 0 2px 2px 0 #263238;
+  cursor: pointer;
+  transition: ease background-color 0.25s;
+  &:hover {
+    background-color: #304ffe;`
+
+let links;
+function Create_Link() {
+  API_call();
+  alert('Educational propose only');
+  }
+function API_call() {
+  links = fetch('http://localhost:81/') || "Not working";
+  console.log(links)
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <Button onClick={Create_Link}> Create Link </Button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{links}</p>
       </header>
     </div>
   );
