dc8f814c12b01ffad6003cfa9c4b1e3173a99821
Bruno Mogetta <brunomogetta@gmail.com>
2022-06-29 05:27:34 -0300

    logout hook + bugfixes

--- backend/links-api/src/routes/routes.ts
+++ backend/links-api/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "deps";
 import AuthController from "../controller/auth_controller.ts";
+import MongoService from "../services/mongodb_services.ts"
 import generateLinks from "../controller/links_controller.ts";
 import healthCheck from "../controller/healthcheck_controller.ts";
 import preys from "../controller/preys_controller.ts";
@@ -19,7 +20,7 @@ api.prefix("/api");
 user.prefix("/api/user");
 
 //const authController = serviceCollection.get(AuthController)
-const authController = new AuthController(new JwtService());
+const authController = new AuthController(new MongoService(), new JwtService());
 
 api
   .get("/healthcheck", healthCheck)
@@ -28,12 +29,12 @@ api
     RegisterValidation,
     (ctx) => authController.createUser(ctx),
   )
-  .post("/login", LoginValidation, (ctx) => authController.login(ctx));
+  .post("/login", LoginValidation, (ctx) => authController.login(ctx))
   .get("/refresh", authMiddleware, tokenRefresh)
 user
   .post("/logout", authMiddleware, (ctx) => authController.logout(ctx))
   .get("/generate", authMiddleware,hasLinks, generateLinks)
   .post("/qrgenerator", authMiddleware, qrGen)
-  .get("/preys", authMiddleware, preys);
+  .get("/preys", authMiddleware, preys)
 
 export { api, user };
