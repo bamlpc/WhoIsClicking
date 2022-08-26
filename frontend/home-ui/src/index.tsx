c242d864252a094f5fd80d5c952bc2d0e7d9cad9
Bruno Mogetta <brunomogetta@gmail.com>
2022-08-25 22:26:21 -0300

    added volumes to home-ui

--- frontend/home-ui/src/index.tsx
+++ frontend/home-ui/src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 //import i18next from './services/language';
 
-//TODO: fix dropdown select language button not working
+//TODO: [WHOIS-20] fix dropdown select language button not working
 //i18next;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
