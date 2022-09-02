e4725801cb4f6ccbd9d9e5637e05b63ba2052df5
bmogetta <brunomogetta@gmail.com>
2022-09-02 12:01:13 -0300

    added pepper, added make winreset

--- backend/links-api/src/helper/userGenerator.ts
+++ backend/links-api/src/helper/userGenerator.ts
@@ -1,11 +1,10 @@
 import { bcrypt } from "deps";
 import { Login } from "../model/models.ts";
 
-//This function hashes the password
-
-const processedUserData = async (userInformation: Login) => {
+//processedUserData adds pepper and hashes the password
+const processedUserData = async (userInformation: Login, pepper: string) => {
   const username = userInformation.username;
-  const password = userInformation.password;
+  const password = userInformation.password + pepper;
   const salt: string = await bcrypt.genSalt();
   const hashedPassword: string = await bcrypt.hash(password, salt);
 
