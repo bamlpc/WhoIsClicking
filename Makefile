1918c2a2f5c9adae67dc1db34db6e8a2873d4e3b
bmogetta <brunomogetta@gmail.com>
2022-09-25 17:40:35 -0300

    working on scanner bug

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ reset:
 	docker-compose down 
 	docker network prune -f
 	docker image rm whoisclicking_home-ui  \
-	whoisclicking_links-api whoisclicking_nginx whoisclicking_mongo-db
+	whoisclicking_links-api whoisclicking_mongo-db
 	docker volume prune -f
 	sudo rm -rf infra/mongo-db/data/ infra/mongo-db/log/
 	docker-compose up --build
--- frontend/home-ui/src/components/Scanner.tsx
+++ frontend/home-ui/src/components/Scanner.tsx
@@ -3,14 +3,14 @@ import DeviceDetector from "device-detector-js";
 
 const Scan = () => {
     const { innerWidth: width, innerHeight: height } = window;
-    console.log("width",width);
-    console.log("height",height);
+    console.log( {width} );
+    console.log( {height} );
 
     const deviceDetector = new DeviceDetector();
     const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
     const device = deviceDetector.parse(userAgent);
+    console.log( {device} );
     DenoScan();
-    console.log(device);
 }
 
 const DenoScan = async ( ) => {
@@ -24,7 +24,8 @@ const DenoScan = async ( ) => {
       }, []);
     
       console.log(data);*/
-    
+        let fetchData;
+        let responseClone;
         fetch('/api/scanner', {
             method: 'GET', // *GET, POST, PUT, DELETE, etc.
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -34,11 +35,24 @@ const DenoScan = async ( ) => {
             },
             referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url  
         } )
-        .then(response => response.json())
-        .then(data => fetchData = data)
-        .catch(error => {console.log(error)})
+        .then(function (response) {
+          responseClone = response.clone(); // 2
+          return response.json();
+        })
+        .then(function (data) {
+          data =>  fetchData = data
+        }, function (rejectionReason) { // 3
+          console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
+          responseClone.text() // 5
+          .then(function (bodyText) {
+              console.log('Received the following instead of valid JSON:', bodyText); // 6
+          });
+        });
+      //   .then(response => response)
+      //   .then(data =>  fetchData = data)
+      //   .catch(error => {console.log(error)})
 
-    console.log(fetchData);
+      console.log( {fetchData} );
 };
 
 export default Scan
