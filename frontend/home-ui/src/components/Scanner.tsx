07a18c9e1fa6b79cfcd477789449d6341a0ec162
bmogetta <brunomogetta@gmail.com>
2022-09-26 19:44:58 -0300

    formated fetchData response

--- frontend/home-ui/src/components/Scanner.tsx
+++ frontend/home-ui/src/components/Scanner.tsx
@@ -3,20 +3,21 @@ import DeviceDetector from "device-detector-js";
 
 const Scan = () => {
     const { innerWidth: width, innerHeight: height } = window;
+    //TODO: remove log
     console.log( {width} );
     console.log( {height} );
 
     const deviceDetector = new DeviceDetector();
     const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36";
     const device = deviceDetector.parse(userAgent);
+    //TODO: remove log
     console.log( {device} );
     DenoScan();
 }
 
 const DenoScan = async ( ) => {
-    let fetchData;
-    let res;
-    fetch('http://localhost:8000/api/scanner', {
+
+    const fetchData = await fetch('http://localhost:8000/api/scanner', {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
@@ -24,17 +25,24 @@ const DenoScan = async ( ) => {
             'Content-Type': 'application/json'
         },
         referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url  
-    } )
+    })
     .then(response => {
         if(!response.ok) {
-                throw Error('Fail to get links');
-        }
-        console.log(response);
+            throw Error('Fail to get links');
+        } 
+        return response.json();
     })
-    .then(data =>  fetchData = data)
-    .catch(error => {console.log(error)})
+    .then(data =>  {
+        return data})
+    .catch(error => {
+        console.error(error)
+    })
 
-    console.log( {fetchData} );
+    //TODO: remove log
+    console.log(fetchData);
+    
+    return fetchData
+    
 
 };
 
