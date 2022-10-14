5a54014e8d68801570f1137a9a5d3d845dc5d6d7
bmogetta <brunomogetta@gmail.com>
2022-10-14 13:43:22 -0300

    added disable react dev tools

--- frontend/dashboard-ui/src/index.tsx
+++ frontend/dashboard-ui/src/index.tsx
@@ -3,9 +3,14 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
